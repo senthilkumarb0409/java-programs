5248768621f3decbd7360b52b1cf489b537e6cff
senthilkumarb0409 <88877288+senthilkumarb0409@users.noreply.github.com>
2021-08-22 13:50:07 +0530

    Create Student2.java

---
+++ Student2.java
@@ -0,0 +1,23 @@
+class Student2{
+int rollno;
+String name;
+
+void insertRecord(int r,String n){
+rollno=r;
+name=n;
+}
+void displayInformation(){
+System.out.println(rollno+"  "+name);
+}
+public static void main(String args[]){
+Student2 s1=new Student2();
+Student2 s2=new Student2();
+
+s1.insertRecord(132,"Samrat");
+s2.insertRecord(154,"Thirulok");
+
+s1.displayInformation();
+s2.displayInformation();
+
+}
+}
