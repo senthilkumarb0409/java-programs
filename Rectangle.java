91597311a7e0aeeb90321563f0ec77c41344aa58
senthilkumarb0409 <88877288+senthilkumarb0409@users.noreply.github.com>
2021-08-22 15:14:35 +0530

    Create Rectangle.java

---
+++ Rectangle.java
@@ -0,0 +1,21 @@
+class Rectangle{
+int length;
+int width;
+
+void insert(int l,int w){
+length=l;
+width=w;
+}
+void calculateArea(){
+     System.out.println(length*width);}
+public static void main(String args[]){
+    Rectangle r1=new Rectangle();
+    Rectangle r2=new Rectangle();
+
+    r1.insert(4,2);
+    r2.insert(3,4);
+
+    r1.calculateArea();
+    r2.calculateArea();
+}
+}
