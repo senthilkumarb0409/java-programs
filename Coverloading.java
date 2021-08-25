5d45af72286edfa527f2f0aa6689edccea5b2cd2
SENTHIL KUMAR.B <88877288+senthilkumarb0409@users.noreply.github.com>
2021-08-25 18:52:43 +0530

    Add files via upload

---
+++ Coverloading.java
@@ -0,0 +1,46 @@
+class Coverloading{
+  int id;
+  String name;
+  int age;
+  Coverloading(int i, String n){
+  id=i;
+  name=n;
+  }
+  Coverloading(int i,String n,int a){
+  id=i;
+  name=n;
+  age=a;
+  }
+  void display(){
+  System.out.println(id+" "+name+" "+age);}
+  
+  
+   public static void main(String args[]){
+   Coverloading s1=new Coverloading(121,"queen");
+   Coverloading s2=new Coverloading(131,"princess",20);
+ 
+    s1.display();
+    s2.display();
+  }
+} 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
